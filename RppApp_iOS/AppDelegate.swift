3a5bdcfd9bba1d817e1c9f6374892f950c6edfb2
Tohure <cr.htorres@gmail.com>
2016-08-28 23:53:39 -0500

    primeros pasos amazon

--- RppApp_iOS/AppDelegate.swift
+++ RppApp_iOS/AppDelegate.swift
@@ -18,6 +18,40 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
+        application.applicationIconBadgeNumber = 0
+        
+        let readAction = UIMutableUserNotificationAction()
+        readAction.identifier = "READ_IDENTIFIER"
+        readAction.title = "Read"
+        readAction.activationMode = .Foreground
+        readAction.destructive = false
+        readAction.authenticationRequired = true
+
+        let ignoreAction = UIMutableUserNotificationAction()
+        ignoreAction.identifier = "IGNORE_IDENTIFIER"
+        ignoreAction.title = "Ignore"
+        ignoreAction.activationMode = .Background
+        ignoreAction.destructive = false
+        ignoreAction.authenticationRequired = true
+        
+        let deleteAction = UIMutableUserNotificationAction()
+        deleteAction.identifier = "DELETE_IDENTIFIER"
+        deleteAction.title = "Delete"
+        deleteAction.activationMode = .Foreground;
+        deleteAction.destructive = true
+        deleteAction.authenticationRequired = true
+        
+        let messageCategory = UIMutableUserNotificationCategory()
+        messageCategory.identifier = "MESSAGE_CATEGORY"
+        messageCategory .setActions([readAction, ignoreAction, deleteAction], forContext: .Default)
+        messageCategory .setActions([readAction,deleteAction], forContext: .Minimal)
+        
+        let categories = NSSet(objects: messageCategory)
+        
+        let settings = UIUserNotificationSettings(forTypes: [.Alert, .Badge, .Sound,], categories: categories as? Set<UIUserNotificationCategory>)
+        UIApplication.sharedApplication().registerUserNotificationSettings(settings)
+        UIApplication.sharedApplication().registerForRemoteNotifications()
+        
         return true
     }
 
@@ -42,7 +76,49 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    //MARK: Notificaciones 
+    
+    
+    func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
+        NSLog("deviceToken: %@", deviceToken);
+    }
 
-
+    func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
+        NSLog("Failed to register with error : %@", error);
+    }
+    
+    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
+        application.applicationIconBadgeNumber = 0
+        let msg = String(format: "%@", userInfo)
+        createAlert(msg)
+    }
+    
+    func createAlert(mensaje: String) {
+        let alert = UIAlertController(title: "Alerta", message: mensaje, preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        self.window?.rootViewController?.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    func application(application: UIApplication, handleActionWithIdentifier identifier: String?, forLocalNotification notification: UILocalNotification, completionHandler: () -> Void) {
+        
+        /*if ([identifier isEqualToString:@"READ_IDENTIFIER"]){
+            NSString *msg = [NSString stringWithFormat:@"%@", @"read"];
+            [self createAlert:msg];
+        }else if ([identifier isEqualToString:@"DELETE_IDENTIFIER"]){
+            NSString *msg = [NSString stringWithFormat:@"%@", @"delete"];
+            [self createAlert:msg];
+        }*/
+        
+        if identifier == "READ_IDENTIFIER" {
+            let msg = String(format: "%@","read")
+            createAlert(msg)
+        }else if identifier == "DELETE_IDENTIFIER"{
+            let msg = String(format: "%@","delete")
+            createAlert(msg)
+        }
+        
+        completionHandler()
+    }
 }
 
