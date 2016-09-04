470592d198f7b0b18ae5092939562f9739ff5bfc
lmarca <luisfmz26@gmail.com>
2016-09-03 19:12:03 -0500

    pruebas streaming

--- RppApp_iOS/AppDelegate.swift
+++ RppApp_iOS/AppDelegate.swift
@@ -88,6 +88,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
+        // MPNowPlayingInfoCenter
+        UIApplication.sharedApplication().beginReceivingRemoteControlEvents()
+        
         application.applicationIconBadgeNumber = 0
         
         let readAction = UIMutableUserNotificationAction()
@@ -191,6 +194,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        // Saves changes in the application's managed object context before the application terminates.
+        
+        UIApplication.sharedApplication().endReceivingRemoteControlEvents()
+        
     }
     
     //MARK: Notificaciones 
